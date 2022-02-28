47579c5d0a98982b5d99f6ab85e4822ee7368f35
Akrem Hammami <akremhammami@outlook.com>
2022-02-28 12:45:18 +0100

    Add password encryption

--- Repositories/Implementations/InstituteRepository.cs
+++ Repositories/Implementations/InstituteRepository.cs
@@ -1,5 +1,6 @@
 ﻿using E_proc.Models;
 using E_proc.Repositories.Interfaces;
+using E_proc.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_proc.Repositories.Implementations
@@ -7,10 +8,12 @@ namespace E_proc.Repositories.Implementations
     public class InstituteRepository : IInstituteRepository
     {
         private readonly AuthContext _dbContext;
+        private readonly IEncryptionService _encryptionService;
 
-        public InstituteRepository(AuthContext dbContext)
+        public InstituteRepository(AuthContext dbContext, IEncryptionService encryptionService)
         {
             _dbContext = dbContext;
+            _encryptionService = encryptionService;
             
         }
 
@@ -20,9 +23,11 @@ namespace E_proc.Repositories.Implementations
 
             if (foundedUser == null)
             {
+                institute.Password = _encryptionService.Encrypt(institute.Password);
                 Address adr = new Address { countryName = institute.address.countryName, city = institute.address.city, postalCode = institute.address.postalCode, street1 = institute.address.street1, street2 = institute.address.street2 };
                 Representative representative = new Representative { Name=institute.Interlocutor.Name , Phone = institute.Interlocutor.Phone, Position = institute.Interlocutor.Position,SocialSecurityNumber=institute.Interlocutor.SocialSecurityNumber,SocialSecurityNumberDate=institute.Interlocutor.SocialSecurityNumberDate,Email=institute.Interlocutor.Email};
                 Institute x = new Institute { Email = institute.Email , FirstName = institute.FirstName, LastName = institute.LastName, Password = institute.Password, Type = institute.Type,AreaType = institute.AreaType , Fax = institute.Fax,NameAr =institute.NameAr, NameFr=institute.NameFr,NotificationEmail=institute.NotificationEmail ,Phone = institute.Phone, representativeName= institute.representativeName,Interlocutor=representative,address=adr };
+           
                 var user = await _dbContext.Institute.AddAsync(x);
                 _dbContext.SaveChanges();
 
@@ -74,8 +79,8 @@ namespace E_proc.Repositories.Implementations
                 if (foundedUser?.Id == id || foundedUser == null)
                 {
 
+                    institute.Password = _encryptionService.Encrypt(institute.Password);
 
-                 
                     oldUser.address.countryName = institute.address.countryName; oldUser.address.city = institute.address.city; oldUser.address.postalCode = institute.address.postalCode; oldUser.address.street1 = institute.address.street1; oldUser.address.street2 = institute.address.street2 ;
                     oldUser.Interlocutor.Name = institute.Interlocutor.Name; oldUser.Interlocutor.Phone = institute.Interlocutor.Phone; oldUser.Interlocutor.Position = institute.Interlocutor.Position; oldUser.Interlocutor.SocialSecurityNumber = institute.Interlocutor.SocialSecurityNumber; oldUser.Interlocutor.SocialSecurityNumberDate = institute.Interlocutor.SocialSecurityNumberDate; oldUser.Interlocutor.Email = institute.Interlocutor.Email ;
                     oldUser.Email = institute.Email; oldUser.FirstName = institute.FirstName; oldUser.LastName = institute.LastName; oldUser.Password = institute.Password; oldUser.Type = institute.Type; oldUser.AreaType = institute.AreaType; oldUser.Fax = institute.Fax;oldUser.NameAr = institute.NameAr; oldUser.NameFr = institute.NameFr; oldUser.NotificationEmail = institute.NotificationEmail; oldUser.Phone = institute.Phone; oldUser.representativeName = institute.representativeName ;
---
+++ Services/Interfaces/IEncryptionService.cs
@@ -0,0 +1,10 @@
+﻿namespace E_proc.Services.Interfaces
+{
+    public interface IEncryptionService
+    {
+        public  string Encrypt(string text);
+        public  string Decrypt(string criptedtext);
+
+
+    }
+}
