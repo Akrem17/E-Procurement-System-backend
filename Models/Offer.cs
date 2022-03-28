7fcfec184f75cb9f3c4184be50039e1375e23b3f
Akrem Hammami <akremhammami@outlook.com>
2022-03-28 17:05:07 +0100

    Add File Data Repository

---
+++ Models/Offer.cs
@@ -0,0 +1,15 @@
+﻿namespace E_proc.Models
+{
+    public class Offer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Tender Tender {get; set ;}
+        public Supplier Supplier { get; set; }
+
+
+ 
+    } 
+}
