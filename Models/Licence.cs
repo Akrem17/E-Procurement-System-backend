cf2b126b148a41a20912462df28409896fb8a23f
Akrem Hammami <akremhammami@outlook.com>
2022-04-08 13:41:26 +0100

    Edit supplier profile

--- Models/Licence.cs
+++ Models/Licence.cs
@@ -6,7 +6,7 @@ namespace E_proc.Models
     public class Licence
     {
         [Key]
-        [JsonIgnore]
+       
         public int Id { get; set; }
         public string Category { get; set; }
         public string Name { get; set; }
