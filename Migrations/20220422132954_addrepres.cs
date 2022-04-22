b70844ef118250cf53628ba0ee13dc5b265dac68
Akrem Hammami <akremhammami@outlook.com>
2022-04-22 15:04:26 +0100

    Add offer representative

---
+++ Migrations/20220422132954_addrepres.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_proc.Migrations
+{
+    public partial class addrepres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RepresentativeId",
+                table: "Offer",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offer_RepresentativeId",
+                table: "Offer",
+                column: "RepresentativeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Offer_Representative_RepresentativeId",
+                table: "Offer",
+                column: "RepresentativeId",
+                principalTable: "Representative",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Offer_Representative_RepresentativeId",
+                table: "Offer");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Offer_RepresentativeId",
+                table: "Offer");
+
+            migrationBuilder.DropColumn(
+                name: "RepresentativeId",
+                table: "Offer");
+        }
+    }
+}
